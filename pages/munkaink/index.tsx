662da05595070c6b1686420514c643d6f8f7c515
KisLajos <64649246+KisLajos@users.noreply.github.com>
2023-05-04 21:18:13 +0200

    Added index files on all pages

---
+++ pages/munkaink/index.tsx
@@ -0,0 +1,21 @@
+import { GetServerSideProps } from 'next';
+
+const Munkaink = () => {
+    return (
+        <div>
+            Enter
+        </div>
+    );
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+
+
+    return {
+        props:{
+
+        }
+    }
+}
+
+export default Munkaink
