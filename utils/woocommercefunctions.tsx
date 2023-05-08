3e836d185474dff960348b98aa844a8cf02e7443
KisLajos <64649246+KisLajos@users.noreply.github.com>
2023-05-08 18:02:22 +0200

    Added logging

---
+++ utils/woocommercefunctions.tsx
@@ -0,0 +1,7 @@
+export async function getProducts() {
+    console.log('Getting products...')
+    /* const resProducts = await fetch(`https://finalexamproject-wp.lajoskis.dk/wp-json/wp/v2/`)
+    const products = await resProducts.json();
+
+    return products; */
+}
--- utils/wordpressfunctions.tsx
+++ utils/wordpressfunctions.tsx
@@ -1,4 +1,5 @@
 export async function getPageDataBySlug(slug: string) {
+    console.log('Getting page by slug: ' + slug);
     const resPage = await fetch(`https://finalexamproject-wp.lajoskis.dk/wp-json/wp/v2/pages?slug=${slug}`)
     const pageDataArray = await resPage.json();
     const page = pageDataArray[0];
@@ -7,6 +8,7 @@ export async function getPageDataBySlug(slug: string) {
 }
 
 export async function getAllPages() {
+    console.log('Getting pages...')
     const resPage = await fetch(`https://finalexamproject-wp.lajoskis.dk/wp-json/wp/v2/pages`)
     const pages = await resPage.json();
 
