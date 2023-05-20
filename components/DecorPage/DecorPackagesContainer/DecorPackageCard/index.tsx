73ed4574de0373af9aea63f01d8f7132ac97fb69
KisLajos <64649246+KisLajos@users.noreply.github.com>
2023-05-20 10:38:10 +0200

    DecorPackage card structure

--- components/DecorPage/DecorPackagesContainer/DecorPackageCard/index.tsx
+++ components/DecorPage/DecorPackagesContainer/DecorPackageCard/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import CardHeader from './CardHeader'
+import CardBody from './CardBody'
+import CardFooter from './CardFooter'
 
 export default function DecorPackageCard() {
   return (
-    <div>DecorPackageCard</div>
+    <div>
+      <CardHeader/>
+      <CardBody/>
+      <CardFooter/>
+    </div>
   )
 }
