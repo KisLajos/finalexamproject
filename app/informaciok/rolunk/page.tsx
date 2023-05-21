d583a5cd8f2b193a6b481f4811ba2d50f33f2574
KisLajos <64649246+KisLajos@users.noreply.github.com>
2023-05-21 20:59:31 +0200

    About Us component fixes

--- app/informaciok/rolunk/page.tsx
+++ app/informaciok/rolunk/page.tsx
@@ -1,22 +1,15 @@
+import AboutUs from "@/components/AboutUs";
 import { getPageDataBySlug } from "@/utils/wordpressfunctions";
 import parse from "html-react-parser";
 
 export async function Rolunk() {
-    const page = await getPageDataBySlug("rolunk")
+    
     //console.log(page)
 
     return (
         <div>
-            <h1>{parse(page.title.rendered)}</h1>
-            <section>
-                <div>
-                    {parse(page.acf.text_block_1)}
-                </div>
-
-                <div>
-                    {parse(page.acf.text_block_2)}
-                </div>
-            </section>
+            <h1>Információk – Rólunk</h1>
+            <AboutUs/>
         </div>
     );
 }
